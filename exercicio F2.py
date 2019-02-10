c37f1ec23f7a02235fdf9e108a8be3e83c984eaf
Victor Augusto de Souza Fonseca <41022135+VictorASF@users.noreply.github.com>
2019-02-09 22:34:30 -0200

    Delete exercicio F2.py

--- exercicio F2.py
+++
@@ -1,6 +0,0 @@
-n1 = int(input('Qual a velocidade do carro em Km/h: '))
-
-if n1>80:
-    print('Foi multado otario, agora tu vai pagar {} temers'.format((n1-80)*5))
-else:
-    print('Passou suave mas na proxima seu dinheiro é meu')
